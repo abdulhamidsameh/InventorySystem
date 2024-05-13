fcc037b18f312bd993f3e1998993f08465ad0628
abdulhamidsameh <abdulhamid.sameh26@gmail.com>
2024-05-13 22:13:53 +0300

    Add project files.

---
+++ InventorySystem/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace InventorySystem
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// 1. Add Product
+			// 2. Update Product
+			// 3. View Product (Id ,Name ,Quantity ,Price)
+			// 4. Exit
+			Console.WriteLine("Hello, World!");
+		}
+	}
+}
